254e34e5fa79ce058ec51d0d3bebca3f7245cf9b
wei_lin <wei1234c@gmail.com>
2022-06-28 23:54:59 +0800

    add nth_order.reset_coefficients()

--- codes/sigma/sigma_studio/toolbox/cells/filters/nth_order.py
+++ codes/sigma/sigma_studio/toolbox/cells/filters/nth_order.py
@@ -24,6 +24,12 @@ class NthOrderFilter(Cell):
         self._dsp.parameter_ram.write(bytes_array = bytes_array, address = min(addresses))
 
 
+    def reset_coefficients(self, algorithm_idx = 0):
+        addresses = tuple(p.address for p in self._module._algorithms[algorithm_idx].parameters.values())
+        values = [1.0, 0.0, 0.0, 0.0, 0.0] * (len(addresses) // 5)
+        self.set_coefficients_values(values, algorithm_idx = algorithm_idx)
+
+
 
 class NthOrderDouble(NthOrderFilter):
     pass
--- codes/sigma/sigma_studio/toolbox/cells/volume_controls/mute.py
+++ codes/sigma/sigma_studio/toolbox/cells/volume_controls/mute.py
@@ -10,6 +10,11 @@ class Mute(Cell):
     ENABLE_PARAM = 'mute'
 
 
+    @property
+    def muted(self):
+        return self.get_param(param_name = self.ENABLE_PARAM).value == 0.0
+
+
     def mute(self, value = True, **kwargs):
         self.set_param(0.0 if value else 1.0, param_name = self.ENABLE_PARAM, send_now = True, **kwargs)
 
